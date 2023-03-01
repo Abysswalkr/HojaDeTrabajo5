f80a22d0be756512ce84cf9f12af77a588c756a0
Jose Gramajo <gra22907@uvg.edu.gt>
2023-03-01 17:38:38 -0600

    Function calles "Process", its represents the process in the simulation and the status

--- main.py
+++ main.py
@@ -15,3 +15,17 @@ NumProcesses = [25, 50, 100, 150, 200]
 AmountProcess = 10
 StartTime = 0
 Time = []
+
+def process(Env, Operation, Ram, Inst,  ID, Memory, Instructions, StTime, Processor):
+    global StartTime
+
+    yield Env.timeout*(StTime)
+    Begginingtime = Env.now
+
+    print(
+        f"{ID}, in queue, Status:NEW. Time: {Env.now:.1f}. Amount of RAM required: {Ram}. Amount of RAM available: {Memory.level}"
+    )
+    yield Memory.get(Ram)
+    print(
+        f"{ID}, in queue, Status:READY. Time: {Env.now:.1f}. Amount of instructions in queue: {Instructions}"
+    )
