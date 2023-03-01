0662156b8c7dabd7f22ca2b1ed305af5de54ec07
Jose Gramajo <gra22907@uvg.edu.gt>
2023-03-01 17:17:02 -0600

    Parameters used in the simulation and the title

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import random
 import simpy
 import numpy
+
+texto = "---Course simulation---"
+ancho = 100
+texto_cent = texto.center(ancho)
+print(texto_cent)
+
+Interval = 10
+RamCapacity = 100
+PROCESS_MEMORY = 10
+CpuSpeed = 3
+NumProcesses = [25, 50, 100, 150, 200]
+AmountProcess = 10
+StartTime = 0
+Time = []
